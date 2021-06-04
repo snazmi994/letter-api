8ec9b1838ca38dbff6c6bbd0ee20d1e827ea0a86
Shan Nazmi <shannazmi@Shans-Mac-mini.attlocal.net>
2021-06-04 14:42:13 -0500

    Update Curl scripts

--- curl-scripts/letters/index.sh
+++ curl-scripts/letters/index.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 API="http://localhost:4741"
-URL_PATH="/letter"
+URL_PATH="/letters"
 
-curl "${API}${URL_PATH}/${ID}" \
+curl "${API}${URL_PATH}" \
   --include \
   --request GET \
   --header "Authorization: Bearer ${TOKEN}"
